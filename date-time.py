c978f7181747d6cdce9bd3585d71c5db8cf7443b
KnightCoder27 <98467710+KnightCoder27@users.noreply.github.com>
2022-03-06 19:44:19 +0530

    Create date-time.py
    Python programs for Datetime and Time Module.

---
+++ date-time.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+today = date.today()
+
+print("Today's date is", today)
+print("Current year:", today.year)
+print("Current month:", today.month)
+print("Current day:", today.day)
+
+import time
+
+print(time.time())
+print(time.sleep(2))
+print(time.ctime())
