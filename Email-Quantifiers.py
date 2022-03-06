cc110c9390be4dc843263ca302e7853bc805fc8c
KnightCoder27 <98467710+KnightCoder27@users.noreply.github.com>
2022-03-06 19:37:03 +0530

    Create Email-Quantifiers.py
    Extracting Mail ID from a String using REGEX in Python.

---
+++ Email-Quantifiers.py
@@ -0,0 +1,16 @@
+# Email Extraction
+
+import re
+s = " Knock, knock … Who’s there? … *very long pause* xyz@gmail.com and abc@gmail.com"
+t = re.findall('\S+@\S+',s)
+print(t)
+
+# Quantifiers
+
+import re
+
+p = re.compile('[0-9]')
+print(p.findall("I went to him at 11 A.M. on 4th July 1886"))
+
+p = re.compile('[0-9]+')
+print(p.findall("I went to him at 11 A.M. on 4th July 1886"))
