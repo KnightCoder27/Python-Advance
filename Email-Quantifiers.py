351a56463787df3536eb136f36a995d803662b15
S Krish <98467710+KnightCoder27@users.noreply.github.com>
2022-03-09 18:01:47 +0530

    Update Email-Quantifiers.py

--- Email-Quantifiers.py
+++ Email-Quantifiers.py
@@ -12,5 +12,5 @@ import re
 p = re.compile('[0-9]')
 print(p.findall("I went to him at 11 A.M. on 4th July 1886"))
 
-p = re.compile('[0-9]+')
-print(p.findall("I went to him at 11 A.M. on 4th July 1886"))
+#p = re.compile('[a-z]+')
+print(re.findall('[a-z]+',"I went to him at 11 A.M. on 4th July 1886"))
